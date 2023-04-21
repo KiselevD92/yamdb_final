077c1768d2a9db16cc0474685dface3274f3b310
KiselevD92 <noproblem2000@yandex.ru>
2023-04-21 12:50:09 +0300

    isort1.1

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from users.models import User
 from reviews.models import Category, Genre, Title
+from users.models import User
 
 
 class TitleAdmin(admin.ModelAdmin):
